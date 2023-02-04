2f826238db8ecacf7ab67ec3bbb3b05e247abd6d
Robert Vari <mail.robertvari@gmail.com>
2023-02-04 17:12:35 +0100

    update

--- 13_property.py
+++ 13_property.py
@@ -2,9 +2,22 @@ class Dice:
     def __init__(self, sides, color):
         self.__sides = sides
         self.__color = color
+    
+    @property
+    def sides(self):
+        return self.__sides
+
+    @property
+    def color(self):
+        return self.__color
+    
+    @color.setter
+    def color(self, new_color):
+        self.__color = new_color
 
     def __str__(self) -> str:
         return f"Sides: {self.__sides} Color: {self.__color}"
 
 dice6 = Dice(6, "white")
-print(dice6)
+dice6.color = "black"
+print(dice6.color)
