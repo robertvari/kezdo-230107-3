a7fe8d680dbb7c3aeb12ce42f57fce7eff38b1d6
Robert Vari <mail.robertvari@gmail.com>
2023-02-04 14:18:13 +0100

    update

---
+++ 02_class_attributes.py
@@ -0,0 +1,18 @@
+class Dice:
+    # class attributes
+    sides = 6
+    color = "white"
+
+
+class Car:
+    # class attributes
+    doors = 5
+    color = "red"
+    max_speed = 130
+
+
+class Person:
+    # class attributes
+    name = "Robert"
+    age = 30
+    address = "Budapest"
