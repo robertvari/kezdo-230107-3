164d7926146c4dba3f97ec09b3a1c132d6465a5a
Robert Vari <mail.robertvari@gmail.com>
2023-02-04 17:24:15 +0100

    update

--- 13_property.py
+++ 13_property.py
@@ -18,6 +18,45 @@ class Dice:
     def __str__(self) -> str:
         return f"Sides: {self.__sides} Color: {self.__color}"
 
-dice6 = Dice(6, "white")
-dice6.color = "black"
-print(dice6.color)
+class Student:
+    def __init__(self, first_name, last_name):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__marks = []
+
+    @property
+    def first_name(self):
+        return self.__first_name
+    
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @property
+    def full_name(self):
+        return f"{self.__first_name} {self.__last_name}"
+
+    @property
+    def student_marks(self):
+        return self.__marks
+
+    @property
+    def avarage(self):
+        return sum(self.__marks) / len(self.__marks)
+
+    def add_mark(self, value):
+        self.__marks.append(value)
+
+    def __repr__(self):
+        return f"{self.__first_name} {self.__last_name}"
+
+stevie = Student("Stevie", "Powers")
+print(stevie)
+stevie.add_mark(1)
+stevie.add_mark(3)
+stevie.add_mark(2)
+stevie.add_mark(2)
+stevie.add_mark(1)
+stevie.add_mark(1)
+print(stevie.student_marks)
+print(stevie.avarage)
